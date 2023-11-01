677e2cbf7dbdf51b2a4ec01d083627d2865157bb
kodalli <surya.kodali@gmail.com>
2023-11-01 02:33:14 -0400

    only using c features

--- main.c
+++ main.c
@@ -1,39 +1,31 @@
 #include <GL/glew.h>
 #include <GLFW/glfw3.h>
-#include <math.h> 
+#include <math.h>
 
-const char* vertexShaderSource = R"(
-#version 330 core
-layout (location = 0) in vec3 aPos;
-uniform mat4 transform;
-out vec3 vertexColor;  // Output variable to transfer vertex position to fragment shader
-void main() {
-    gl_Position = transform * vec4(aPos, 1.0);
-    vertexColor = aPos;  // Transfer vertex position
-}
-)";
+const char* vertexShaderSource =
+"#version 330 core\n"
+"layout (location = 0) in vec3 aPos;\n"
+"uniform mat4 transform;\n"
+"out vec3 vertexColor;\n"
+"void main() {\n"
+"    gl_Position = transform * vec4(aPos, 1.0);\n"
+"    vertexColor = aPos;\n"
+"}\n";
 
-const char* fragmentShaderSource = R"(
-#version 330 core
-in vec3 vertexColor;  // Input vertex position from vertex shader
-out vec4 FragColor;
-void main() {
-    FragColor = vec4(abs(vertexColor), 1.0);  // Use the absolute value of the vertex position as color
-}
-)";
-
-
+const char* fragmentShaderSource =
+"#version 330 core\n"
+"in vec3 vertexColor;\n"
+"out vec4 FragColor;\n"
+"void main() {\n"
+"    FragColor = vec4(abs(vertexColor), 1.0);\n"
+"}\n";
 
 void generateRotationMatrix(float angle, float matrix[4][4]) {
-    // Rotation around X-axis
     float cosAx = cos(angle);
     float sinAx = sin(angle);
-
-    // Rotation around Y-axis
     float cosAy = cos(angle * 0.5f);
     float sinAy = sin(angle * 0.5f);
 
-    // Matrix for X-axis rotation
     float rotationX[4][4] = {
         {1.0f, 0.0f, 0.0f, 0.0f},
         {0.0f, cosAx, -sinAx, 0.0f},
@@ -41,7 +33,6 @@ void generateRotationMatrix(float angle, float matrix[4][4]) {
         {0.0f, 0.0f, 0.0f, 1.0f}
     };
 
-    // Matrix for Y-axis rotation
     float rotationY[4][4] = {
         {cosAy, 0.0f, sinAy, 0.0f},
         {0.0f, 1.0f, 0.0f, 0.0f},
@@ -49,7 +40,6 @@ void generateRotationMatrix(float angle, float matrix[4][4]) {
         {0.0f, 0.0f, 0.0f, 1.0f}
     };
 
-    // Multiply together to get combined rotation matrix
     for (int i = 0; i < 4; i++) {
         for (int j = 0; j < 4; j++) {
             matrix[i][j] = 0.0f;
@@ -60,10 +50,7 @@ void generateRotationMatrix(float angle, float matrix[4][4]) {
     }
 }
 
-
-
 int main() {
-    // Initialization
     glfwInit();
     GLFWwindow* window = glfwCreateWindow(800, 600, "Cube Renderer", NULL, NULL);
     glfwMakeContextCurrent(window);
@@ -71,7 +58,6 @@ int main() {
     glViewport(0, 0, 800, 600);
     glEnable(GL_DEPTH_TEST);
 
-    // Shader creation
     GLuint vertexShader = glCreateShader(GL_VERTEX_SHADER);
     glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
     glCompileShader(vertexShader);
@@ -85,7 +71,6 @@ int main() {
     glDeleteShader(vertexShader);
     glDeleteShader(fragmentShader);
 
-    // Vertex data
     float vertices[] = {
        -0.5f, -0.5f, -0.5f,
         0.5f, -0.5f, -0.5f,
@@ -120,7 +105,6 @@ int main() {
 
     float rotationMatrix[4][4];
 
-    // Rendering loop
     while (!glfwWindowShouldClose(window)) {
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
@@ -129,7 +113,7 @@ int main() {
         glUseProgram(shaderProgram);
         glUniformMatrix4fv(glGetUniformLocation(shaderProgram, "transform"), 1, GL_FALSE, &rotationMatrix[0][0]);
         glBindVertexArray(VAO);
-        glDrawElements(GL_TRIANGLES, sizeof(indices) / sizeof(unsigned int), GL_UNSIGNED_INT, 0);
+        glDrawElements(GL_TRIANGLES, 36, GL_UNSIGNED_INT, 0);
 
         glfwSwapBuffers(window);
         glfwPollEvents();
@@ -138,4 +122,5 @@ int main() {
     glfwTerminate();
     return 0;
 }
+
 
